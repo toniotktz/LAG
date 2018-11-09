048792184dc445289f8ad30176c893a863e41f9f
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-09 22:46:48 +0100

    Ajoutez des fichiers projet.

---
+++ LAG/NousContacter.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Association Landes Arena Gaming</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <style>
+        .fakeimg {
+            height: 200px;
+            background: #aaa;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <h1>Landes Arena Gaming</h1>
+    </div>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="Index.php">LAG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="Newletter.php">Newsletter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="NousContacter.php">Nous Contacter</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container" style="margin-top:30px">
+        <div class="col-sm-12 text-center">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Landes Arena Gaming</h4>
+                    <p class="card-text">Association</p>
+                    <p>landes-arena-gaming@outlook.fr</p>
+                </div>
+            </div>
+        </div>
+        <br />
+    </div>
+        <div class="container" style="margin-top:30px">
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <div class="card">
+                        <div class="card-body">
+                            <img class="card-img-top" src="Images\yugioh_Home.jpg"/>
+                            <h4 class="card-title">Antoine PESTEL</h4>
+                            <p class="card-text">Trésorier</p>
+                            <p>antoine.pestel@live.fr</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Laurent BLANC</h4>
+                            <p class="card-text">Président</p>
+                            <p>mail</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Sébastien Diouf</h4>
+                            <p class="card-text">Secrétaire</p>
+                            <p>mail</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+        </div>
+
+
+
