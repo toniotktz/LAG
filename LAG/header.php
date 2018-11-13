29757fd1065b083375576ebc12a9eaafcebcc6ba
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-13 13:19:46 +0100

    bootstrap

--- LAG/header.php
+++ LAG/header.php
@@ -3,12 +3,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="CSS\Stylesheet1.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS\bootstrap.min.css">
 
     <!-- Importation bootstrapp -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="js\bootstrap.bundle.min.js"></script>
+    <script src="js\jquery.easing.min.js"></script>
+    <script src="js\jquery.min.js"></script>
     <style>
         .fakeimg {
             height: 200px;
