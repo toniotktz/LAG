bec233b9aa4b0eb46874c8cef535bef95ac46db1
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-13 11:42:13 +0100

    création du header et footer dans d'autre fichier et implémentation

---
+++ LAG/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <a href="https://www.youtube.com/channel/UCqG2o8Nm08YVKgXlUFMY0Cw"><img class="lien_social" src="images\youtube.png" /></a>
+        <a href="https://www.twitch.tv/landes_arena_gaming"><img class="lien_social" src="images\twitch.png" /></a>
+        <a href="https://www.instagram.com/laglandes/?hl=fr"><img class="lien_social" src="images\insta.png" /></a>
+        <a href="https://www.facebook.com/LandesArenaGaming/"><img class="lien_social" src="images\facebook.png" /></a>
+        <a href="https://twitter.com/ArenaLandes"><img class="lien_social" src="images\twitter.png" /></a>
+    </div>
+</footer>
---
+++ LAG/header.php
@@ -0,0 +1,37 @@
+<head>
+    <title>Association Landes Arena Gaming</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="CSS\Stylesheet1.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Importation bootstrapp -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <style>
+        .fakeimg {
+            height: 200px;
+            background: #aaa;
+        }
+    </style>
+        <div class="jumbotron text-center" style="margin-bottom:0">
+        <h1>Landes Arena Gaming</h1>
+    </div>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="Index.php">LAG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="Newletter.php">Newsletter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="NousContacter.php">Nous Contacter</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</head>
