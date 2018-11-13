b54906278a0da109f15c13eaef4c4cf8c0c61da6
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-13 23:27:03 +0100

    ajout discord

--- LAG/header.php
+++ LAG/header.php
@@ -9,7 +9,7 @@
     <script src="js\bootstrap.bundle.min.js"></script>
     <script src="js\jquery.easing.min.js"></script>
     <script src="js\jquery.min.js"></script>
-        <img class="banniere" src="images\banniere.png"/>
+    <img class="banniere" src="images\banniere.png" />
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <a class="navbar-brand" href="Index.php">LAG</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -26,11 +26,12 @@
             </ul>
         </div>
         <div class="my-2 my-sm-0">
-        <a href="https://www.instagram.com/laglandes/?hl=fr"><img class="lien_social" src="images\48\insta.png" /></a>
-        <a href="https://www.facebook.com/LandesArenaGaming/"><img class="lien_social" src="images\48\facebook.png" /></a>
-        <a href="https://twitter.com/ArenaLandes"><img class="lien_social" src="images\48\twitter.png" /></a>
-        <a href="https://www.twitch.tv/landes_arena_gaming"><img class="lien_social" src="images\48\twitch.png" /></a>
-        <a href="https://www.youtube.com/channel/UCqG2o8Nm08YVKgXlUFMY0Cw"><img class="lien_social" src="images\48\youtube.png" /></a>
+            <a href="https://www.instagram.com/laglandes/?hl=fr"><img class="lien_social" src="images\48\insta.png" /></a>
+            <a href="https://www.facebook.com/LandesArenaGaming/"><img class="lien_social" src="images\48\facebook.png" /></a>
+            <a href="https://twitter.com/ArenaLandes"><img class="lien_social" src="images\48\twitter.png" /></a>
+            <a href="https://discord.gg/zbwECKn"><img class="lien_social" src="images\48\discord.png" /></a>
+            <a href="https://www.twitch.tv/landes_arena_gaming"><img class="lien_social" src="images\48\twitch.png" /></a>
+            <a href="https://www.youtube.com/channel/UCqG2o8Nm08YVKgXlUFMY0Cw"><img class="lien_social" src="images\48\youtube.png" /></a>
         </div>
     </nav>
 </header>
