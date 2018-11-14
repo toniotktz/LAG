c8cd15c180f072bd3bda60c8b6c39c1c2be5697c
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-14 22:17:51 +0100

    oui

--- LAG/formInscription.php
+++ LAG/formInscription.php
@@ -15,6 +15,10 @@
                         <label for="Prenom">Prenom:</label>
                         <input type="text" class="form-control" id="prenom" placeholder="Entrer votre Prenom" name="prenom" />
                     </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Entrer votre Email" name="email" />
+                    </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="form-group">
@@ -25,16 +29,10 @@
                         <label for="codePostal">Code postal:</label>
                         <input type="text" class="form-control" id="codePostal" placeholder="Entrer votre code postal" name="codePostal" />
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-sm-6">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Entrer votre Email" name="email" />
-                </div>
-                <div class="form-group col-sm-6">
-                    <label for="tel">Telephone:</label>
-                    <input type="tel" class="form-control" id="tel" placeholder="Entrer votre numero de telephone" name="tel" />
+                    <div class="form-group">
+                        <label for="tel">Telephone:</label>
+                        <input type="tel" class="form-control" id="tel" placeholder="Entrer votre numero de telephone" name="tel" />
+                    </div>
                 </div>
             </div>
         </form>
