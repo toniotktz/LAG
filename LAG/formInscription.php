22ed404cab1778a5fcbc71fd4968689c5b5fe1cd
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-15 10:55:08 +0100

    test verif form

--- LAG/formInscription.php
+++ LAG/formInscription.php
@@ -1,46 +1,50 @@
 <!DOCTYPE html>
 <html lang="fr">
-<?php require "header.php"?>
+<?php require "header.php";?>
 <body>
     <div class="container" style="margin-top:30px">
         <h1>Formulaire d'inscription</h1>
-        <form>
+        <form method="post">
             <div class="row">
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <label for="Nom">Nom:</label>
-                        <input type="text" class="form-control" id="Nom" placeholder="Entrer votre nom" name="nom" />
-                    </div>
-                    <div class="form-group">
-                        <label for="Prenom">Prenom:</label>
-                        <input type="text" class="form-control" id="prenom" placeholder="Entrer votre Prenom" name="prenom" />
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" class="form-control" id="email" placeholder="Entrer votre Email" name="email" />
-                    </div>
+                <div class="form-group col-sm-6">
+                    <label for="Nom">Nom:</label>
+                    <input type="text" class="form-control" id="nom" placeholder="Entrer votre nom" name="nom" />
+
+                    <label for="Prenom">Prenom:</label>
+                    <input type="text" class="form-control" id="prenom" placeholder="Entrer votre Prenom" name="prenom" />
+
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Entrer votre Email" name="email" />
                 </div>
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <label for="ville">Ville:</label>
-                        <input type="text" class="form-control" id="prenom" placeholder="Entrer la ville ou vous habitez" name="ville" />
-                    </div>
-                    <div class="form-group">
-                        <label for="codePostal">Code postal:</label>
-                        <input type="text" class="form-control" id="codePostal" placeholder="Entrer votre code postal" name="codePostal" />
-                    </div>
-                    <div class="form-group">
-                        <label for="tel">Telephone:</label>
-                        <input type="tel" class="form-control" id="tel" placeholder="Entrer votre numero de telephone" name="tel" />
-                    </div>
+                <div class="form-group col-sm-6">
+                    <label for="ville">Ville:</label>
+                    <input type="text" class="form-control" id="prenom" placeholder="Entrer la ville ou vous habitez" name="ville" />
+
+                    <label for="codePostal">Code postal:</label>
+                    <input type="text" class="form-control" id="cp" placeholder="Entrer votre code postal" name="cp" />
+
+                    <label for="tel">Telephone:</label>
+                    <input type="tel" class="form-control" id="tel" placeholder="Entrer votre numero de telephone" name="tel" />
                 </div>
             </div>
+            <input type="submit" class="btn btn-primary" name="confirmer" value="Confirmer" />
         </form>
-        <div class="text-center">
-            <button type="button" class="btn btn-primary">Confirmer</button>
-        </div>
-        
     </div>
 </body>
-<?php require "footer.php"?>
+<?php require "footer.php";?>
 </html>
+<?php
+if(isset($_POST['confirmer'])){
+    if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['email']) || empty($_POST['ville']) || empty($_POST['cp']) || empty($_POST['tel'])){
+        echo "Veuillez remplir tout les champs";
+    }
+    else{
+        $nom=$_POST['nom'];
+        $prenom=$_POST['prenom'];
+        $email=$_POST['email'];
+        $ville=$_POST['ville'];
+        $cp=$_POST['cp'];
+        $tel=$_POST['tel'];
+    }
+}
+?>
