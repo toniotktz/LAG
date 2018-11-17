bbc22cb9979bc8f8928b88f701bc150f8dbe0730
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-17 02:23:16 +0100

    zzz

--- LAG/pdo/ajoutInscription.php
+++ LAG/pdo/ajoutInscription.php
@@ -1,23 +1,4 @@
 <?php include "database.php"; ?>
-<?php
-/*$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$email = $_POST['email'];
-$ville = $_POST['ville'];
-$cp = $_POST['cp'];
-$telephone = $_POST['telephone'];*/
 
-$req = $conn->prepare('INSERT INTO inscription(nom, prenom, email, ville, cp, telephone)VALUES(:nom, :prenom, :emial, :ville, :cp, :telephone)')or exit(print_r($conn->errorInfo()));;
-$req->execute(array(
-    'nom' => $nom,
-    'prenom' => $prenom,
-    'email' => $email,
-    'ville' => $ville,
-    'cp' => $cp,
-    'telephone' => $telephone
-    ));
-header('Location: ..\index.php');
-
-?>
 
 
--- LAG/pdo/database.php
+++ LAG/pdo/database.php
@@ -1,16 +1,1 @@
-<?php
-$serveur ='localhost';
-$login ='root';
-$mdp ='';
-$nom_bdd='lag';
-
-try
-{
-    $conn = new PDO('mysql:host='.$serveur.';dbname='.$nom_bdd.'', $login, $mdp);
-
-}
-catch (Exception $e)
-{
-    exit('Erreur : ' . $e->getMessage());
-}
-?>
+<?php ?>
