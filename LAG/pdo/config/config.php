6587efc16c439d080e2b2fcfa98717f3403e8ba2
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-17 15:41:34 +0100

    laurent t nul

--- LAG/pdo/config/config.php
+++ LAG/pdo/config/config.php
@@ -3,5 +3,5 @@
     const DBS_HOST = 'localhost'; //le chemin vers le serveur
     const DBS_BASE = 'landesArena'; //le nom de votre base de données
     const DBS_USER = 'lag'; //nom d'utilisateur pour se connecter 
-    const DBS_PASS = 'b00ba_fett'; //MDP de l'utilsateur pour se connecter
+    const DBS_PASS = 'B00ba_fett'; //MDP de l'utilsateur pour se connecter
 ?>
