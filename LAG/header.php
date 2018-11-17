4f2dfee2542d6d7416f7e6bb5d5668a12e76105d
ANTOINE PESTEL <antoine.pestel@viacesi.fr>
2018-11-17 19:26:53 +0100

    dossier de mort

--- LAG/header.php
+++ LAG/header.php
@@ -2,14 +2,14 @@
     <title>Association Landes Arena Gaming</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css\Stylesheet1.css">
-    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css/Stylesheet1.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <!-- Importation bootstrapp -->
-    <script src="js\bootstrap.bundle.min.js"></script>
-    <script src="jquery\jquery.easing.min.js"></script>
-    <script src="jquery\jquery.min.js"></script>
-    <img class="banniere" src="images\banniere2.png" />
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="jquery/jquery.easing.min.js"></script>
+    <script src="jquery/jquery.min.js"></script>
+    <img class="banniere" src="images/banniere2.png" />
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <a class="navbar-brand" href="Index.php">LAG</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -29,12 +29,12 @@
             </ul>
         </div>
         <div class="my-2 my-sm-0">
-            <a href="https://www.instagram.com/laglandes/?hl=fr"><img class="lien_social" src="images\32\insta.png" /></a>
-            <a href="https://www.facebook.com/LandesArenaGaming/"><img class="lien_social" src="images\32\facebook.png" /></a>
-            <a href="https://twitter.com/ArenaLandes"><img class="lien_social" src="images\32\twitter.png" /></a>
-            <a href="https://discord.gg/zbwECKn"><img class="lien_social" src="images\32\discord.png" /></a>
-            <a href="https://www.twitch.tv/landes_arena_gaming"><img class="lien_social" src="images\32\twitch.png" /></a>
-            <a href="https://www.youtube.com/channel/UCqG2o8Nm08YVKgXlUFMY0Cw"><img class="lien_social" src="images\48\youtube.png" /></a>
+            <a href="https://www.instagram.com/laglandes/?hl=fr"><img class="lien_social" src=<?=__DIR__."/images/32/insta.png"?> /></a>
+            <a href="https://www.facebook.com/LandesArenaGaming/"><img class="lien_social" src="images/32/facebook.png" /></a>
+            <a href="https://twitter.com/ArenaLandes"><img class="lien_social" src="images/32/twitter.png" /></a>
+            <a href="https://discord.gg/zbwECKn"><img class="lien_social" src="images/32/discord.png" /></a>
+            <a href="https://www.twitch.tv/landes_arena_gaming"><img class="lien_social" src="images/32/twitch.png" /></a>
+            <a href="https://www.youtube.com/channel/UCqG2o8Nm08YVKgXlUFMY0Cw"><img class="lien_social" src="images/48/youtube.png" /></a>
         </div>
     </nav>
 </header>
