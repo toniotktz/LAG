b402d4c24d76ca32c8b36d9c854a20623738c87b
l.blanc <l.blanc@intech-so.fr>
2018-11-17 15:37:02 +0100

    c

--- LAG/pdo/config/config.php
+++ LAG/pdo/config/config.php
@@ -1,7 +1,7 @@
 <?php
     //Constantes pour la base de donnees  
-    const DBS_HOST = 'LAG'; //le chemin vers le serveur
+    const DBS_HOST = 'localhost'; //le chemin vers le serveur
     const DBS_BASE = 'landesArena'; //le nom de votre base de données
-    const DBS_USER = 'root'; //nom d'utilisateur pour se connecter 
-    const DBS_PASS = ''; //MDP de l'utilsateur pour se connecter
+    const DBS_USER = 'lag'; //nom d'utilisateur pour se connecter 
+    const DBS_PASS = 'b00ba_fett'; //MDP de l'utilsateur pour se connecter
 ?>
